51d07d1225986048eaaedd54047deeb7fbbcd9dd
Pablo Gimeno <pablogj@unizar.es>
2019-04-09 22:29:53 +0100

    New plot with some rates

--- unizar_scrapper.py
+++ unizar_scrapper.py
@@ -14,8 +14,8 @@ import numpy as np
 # Get current size
 fig_size = plt.rcParams["figure.figsize"]
 # Set figure width
-fig_size[0] = 14
-fig_size[1] = 10
+fig_size[0] = 16
+fig_size[1] = 12
 plt.rcParams["figure.figsize"] = fig_size
 
 def parse_table_html(table):
@@ -89,8 +89,14 @@ indx = np.arange(len(ofertadas))
 width=0.2
 plt.figure()
 plt.bar(indx - width,ofertadas, width = width)
-plt.bar(indx + width,nuevo_ingreso, width = width)
 plt.bar(indx,matriculados, width = width)
+plt.bar(indx + width,nuevo_ingreso, width = width)
+
+for i in range(len(ofertadas)):
+    plt.text(indx[i] - width - 0.04, ofertadas[i] + 0.3, str(ofertadas[i]))
+    plt.text(indx[i] + width - 0.04, nuevo_ingreso[i] + 0.3, str(nuevo_ingreso[i]))
+    plt.text(indx[i] - 0.04, matriculados[i] + 0.3 , str(matriculados[i]))
+
 plt.gca().set_xticklabels(labels)
 plt.title(titulacion)
 plt.xlabel("Curso academico")
@@ -106,9 +112,8 @@ table_parsed = next(iterate_list)
 table_parsed = next(iterate_list)
 
 #Esta lista contiene duracion media graduados
-table_parsed = next(iterate_list).replace("",0)
+table_parsed = next(iterate_list).replace("","0")
 labels =  table_parsed.loc[1:,0].values
-print(labels)
 duracion_media = np.array(table_parsed.loc[1:,1].values, dtype = np.float)
 
 indx = np.arange(len(duracion_media))
@@ -116,6 +121,10 @@ indx = np.arange(len(duracion_media))
 width=0.4
 plt.figure()
 plt.bar(indx,duracion_media, width = width)
+
+for i in range(len(duracion_media)):
+    plt.text(indx[i] - 0.05, duracion_media[i] + 0.02, str(duracion_media[i]), size = 12)
+
 plt.xticks(indx)
 plt.gca().set_xticklabels(labels)
 plt.title(titulacion)
@@ -124,3 +133,33 @@ plt.ylabel("Años")
 plt.legend(["Duración media graduados"])
 
 plt.savefig("figures_examples/fig_duracion_graduados.png")
+
+
+#Esta tabla contiene tasa de exito, rendimiento y eficiencia
+table_parsed = next(iterate_list).replace("","0")
+
+labels =  table_parsed.loc[1:,0].values
+exito = np.array(table_parsed.loc[1:,1].values, dtype = np.float)
+rendimiento = np.array(table_parsed.loc[1:,2].values, dtype = np.float)
+eficiencia = np.array(table_parsed.loc[1:,3].values, dtype = np.float)
+
+indx = np.arange(len(exito))
+width=0.2
+plt.figure()
+plt.bar(indx - width,exito, width = width)
+plt.bar(indx,rendimiento, width = width)
+plt.bar(indx + width,eficiencia, width = width)
+
+for i in range(len(exito)):
+    plt.text(indx[i] - width - 0.08, exito[i] + 0.4, str(exito[i]) + "%", size=9)
+    plt.text(indx[i] - 0.08 , rendimiento[i] + 0.4, str(rendimiento[i]) + "%", size=9)
+    plt.text(indx[i] + width - 0.08 , eficiencia[i] + 0.4, str(eficiencia[i]) + "%", size=9)
+
+plt.xticks(indx)
+plt.gca().set_xticklabels(labels)
+plt.title(titulacion)
+plt.xlabel("Curso academico")
+plt.ylabel("Porcentaje (%)")
+plt.legend(["Exito","Rendimiento","Eficiencia"], loc = 'lower right')
+
+plt.savefig("figures_examples/fig_tasas.png")
